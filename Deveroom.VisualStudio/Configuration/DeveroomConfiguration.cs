a42da82c99cb2d688ae35c4adf7fc719bb28269d
Gaspar Nagy <gaspar.nagy@gmail.com>
2019-07-01 17:51:22 +0200

    Use https://www.nuget.org/packages/Equ for generating equals

--- Deveroom.VisualStudio/Configuration/DeveroomConfiguration.cs
+++ Deveroom.VisualStudio/Configuration/DeveroomConfiguration.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using Equ;
 
 namespace Deveroom.VisualStudio.Configuration
 {
-    public class DeveroomConfiguration
+    public class DeveroomConfiguration: MemberwiseEquatable<DeveroomConfiguration>
     {
+        [MemberwiseEqualityIgnore]
         public DateTime ConfigurationChangeTime { get; set; } = DateTime.MinValue;
 
         public string ConfigurationBaseFolder { get; set; }
@@ -32,37 +34,5 @@ namespace Deveroom.VisualStudio.Configuration
             SpecFlow.CheckConfiguration();
         }
 
-
-        #region Equality
-
-        protected bool Equals(DeveroomConfiguration other)
-        {
-            return ConfigurationChangeTime.Equals(other.ConfigurationChangeTime) && string.Equals(ConfigurationBaseFolder, other.ConfigurationBaseFolder) && Equals(SpecFlow, other.SpecFlow) && ProcessorArchitecture == other.ProcessorArchitecture && DebugConnector == other.DebugConnector && string.Equals(DefaultFeatureLanguage, other.DefaultFeatureLanguage) && string.Equals(ConfiguredBindingCulture, other.ConfiguredBindingCulture);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((DeveroomConfiguration) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                var hashCode = ConfigurationChangeTime.GetHashCode();
-                hashCode = (hashCode * 397) ^ (ConfigurationBaseFolder != null ? ConfigurationBaseFolder.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (SpecFlow != null ? SpecFlow.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (int) ProcessorArchitecture;
-                hashCode = (hashCode * 397) ^ DebugConnector.GetHashCode();
-                hashCode = (hashCode * 397) ^ (DefaultFeatureLanguage != null ? DefaultFeatureLanguage.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (ConfiguredBindingCulture != null ? ConfiguredBindingCulture.GetHashCode() : 0);
-                return hashCode;
-            }
-        }
-
-        #endregion
     }
 }
--- Deveroom.VisualStudio/Configuration/SpecFlowConfiguration.cs
+++ Deveroom.VisualStudio/Configuration/SpecFlowConfiguration.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Linq;
 using Deveroom.VisualStudio.ProjectSystem.Settings;
+using Equ;
 
 namespace Deveroom.VisualStudio.Configuration
 {
-    public class SpecFlowConfiguration
+    public class SpecFlowConfiguration: MemberwiseEquatable<SpecFlowConfiguration>
     {
         public bool IsSpecFlowProject { get; set; }
 
@@ -22,35 +23,5 @@ namespace Deveroom.VisualStudio.Configuration
         {
             FixEmptyContainers();
         }
-
-        #region Equality
-
-        protected bool Equals(SpecFlowConfiguration other)
-        {
-            return IsSpecFlowProject == other.IsSpecFlowProject && string.Equals(Version, other.Version) && string.Equals(GeneratorFolder, other.GeneratorFolder) && string.Equals(ConfigFilePath, other.ConfigFilePath) && Equals(Traits, other.Traits);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((SpecFlowConfiguration) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                var hashCode = IsSpecFlowProject.GetHashCode();
-                hashCode = (hashCode * 397) ^ (Version != null ? Version.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (GeneratorFolder != null ? GeneratorFolder.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (ConfigFilePath != null ? ConfigFilePath.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (Traits != null ? Traits.GetHashCode() : 0);
-                return hashCode;
-            }
-        }
-
-        #endregion
     }
 }
