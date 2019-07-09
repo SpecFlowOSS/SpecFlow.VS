b40219d61b1fa5ebd80c99d3188a8e0675b522d2
Gaspar Nagy <gaspar.nagy@gmail.com>
2019-07-09 15:16:15 +0200

    remove unused file

--- Tests/Deveroom.VisualStudio.Specs/SpecFlow.xUnitAdapter.Config.cs
+++
@@ -1,4 +0,0 @@
-﻿// This configuration is required by SpecFlow.xUnitAdapter. See https://github.com/gasparnagy/SpecFlow.xUnitAdapter for details.
-
-//TODO: fix SpecFlow.xUnitAdapter for sdk-style projects
-//[assembly: Xunit.TestFramework("SpecFlow.xUnitAdapter.SpecFlowPlugin.SpecFlowTestFramework", "SpecFlow.xUnitAdapter.SpecFlowPlugin")]
