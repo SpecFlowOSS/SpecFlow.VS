8aa277481fa679c8da23258044117d47c80319c2
Gaspar Nagy <gaspar.nagy@gmail.com>
2021-07-28 14:53:05 +0200

    Added MonitoringService

---
+++ Deveroom.VisualStudio/ExceptionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Deveroom.VisualStudio
+{
+    internal static class ExceptionExtensions
+    {
+        public static string GetFlattenedMessage(this Exception ex)
+        {
+            var message = ex.Message;
+            while (ex.InnerException != null)
+            {
+                ex = ex.InnerException;
+                message += "->" + ex.Message;
+            }
+
+            return message;
+        }
+    }
+}
