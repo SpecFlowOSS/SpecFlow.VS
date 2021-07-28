a8aa109efdd91199c36a65590611fe2a2c345035
Gaspar Nagy <gaspar.nagy@gmail.com>
2021-07-28 14:41:05 +0200

    Added EventTracker

---
+++ Deveroom.VisualStudio/EventTracking/AnalyticsApi.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Deveroom.VisualStudio.EventTracking
+{
+    public class AnalyticsApi : IAnalyticsApi
+    {
+        public bool HasClientId
+        {
+            get
+            {
+                //TODO: return RegistryManager.GetStringValue("cid") != null;
+                return false;
+            }
+        }
+        
+        public bool LogEnabled { get; }
+        public Exception SenderError { get; }
+
+        public AnalyticsApi()
+        {
+            var devMode = IsDevMode();
+            LogEnabled = devMode;
+
+            //TODO: Create sender, get SenderError
+        }
+
+        public void Log(object message)
+        {
+            if (!LogEnabled || message == null)
+                return;
+
+            if (message is Exception exceptionMessage)
+            {
+                message = "Error: " + EventTracker.GetExceptionMessage(exceptionMessage);
+            }
+
+            Debug.WriteLine(message, GetType().FullName);
+        }
+
+        private bool IsDevMode()
+        {
+            return true; //TODO
+        }
+        
+        public void TrackEvent(string category, string action, string label, int? value = null)
+        {
+            //TODO
+        }
+
+        public void TrackException(string exceptionDetail, bool isFatal)
+        {
+            //TODO
+        }
+    }
+}
