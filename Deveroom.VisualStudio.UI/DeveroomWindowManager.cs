939da069546942d68d44077b0ef23028d86c344a
Gaspar Nagy <gaspar.nagy@gmail.com>
2021-07-28 14:53:34 +0200

    Fixed WindowManager

--- Deveroom.VisualStudio.UI/DeveroomWindowManager.cs
+++ Deveroom.VisualStudio.UI/DeveroomWindowManager.cs
@@ -1,19 +1,25 @@
 ﻿using System;
+using System.ComponentModel.Composition;
 using System.Linq;
+using System.Windows.Navigation;
+using Deveroom.VisualStudio.EventTracking;
 using Deveroom.VisualStudio.ProjectSystem;
 using Deveroom.VisualStudio.UI.ViewModels;
 using Deveroom.VisualStudio.UI.Dialogs;
+using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 
 namespace Deveroom.VisualStudio.UI
 {
     //https://stackoverflow.com/questions/40608094/wpf-modal-window-in-visual-studio-extension-blocking-input
     //https://stackoverflow.com/questions/38057256/wpf-dialog-blocking-input-after-closing?rq=1
+    [Export(typeof(IDeveroomWindowManager))]
     public class DeveroomWindowManager : IDeveroomWindowManager
     {
         protected readonly IVsUIShell _vsUiShell;
 
-        public DeveroomWindowManager(IServiceProvider serviceProvider)
+        [ImportingConstructor]
+        public DeveroomWindowManager([Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider)
         {
             _vsUiShell = (IVsUIShell)serviceProvider.GetService(typeof(SVsUIShell));
         }
@@ -25,6 +31,22 @@ namespace Deveroom.VisualStudio.UI
         }
 
         protected virtual DialogWindow CreateWindow<TViewModel>(TViewModel viewModel)
+        {
+            var dialogWindow = CreateWindowInternal(viewModel);
+            dialogWindow.LinkClicked += OnLinkClicked;
+            return dialogWindow;
+        }
+
+        private void OnLinkClicked(object sender, RequestNavigateEventArgs e)
+        {
+            var uriString = e.Uri.ToString();
+            if (!uriString.StartsWith("file"))
+            {
+                EventTracker.TrackWelcomeLinkClick(uriString);
+            }
+        }
+
+        private DialogWindow CreateWindowInternal<TViewModel>(TViewModel viewModel)
         {
             if ((object)viewModel is CreateStepDefinitionsDialogViewModel createStepDefinitionsDialogViewModel)
                 return new CreateStepDefinitionsDialog(createStepDefinitionsDialogViewModel, _vsUiShell);
