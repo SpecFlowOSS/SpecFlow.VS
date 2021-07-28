c376a14577273debd37571ba6703b03b87ce1b80
Balázs Epresi <epresib@gmail.com>
2021-07-28 13:59:46 +0200

    Add WelcomeDialog and ViewModels

---
+++ Deveroom.VisualStudio.UI/Dialogs/WelcomeDialog.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Deveroom.VisualStudio.UI.ViewModels;
+using Deveroom.VisualStudio.UI.ViewModels.WizardDialogs;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Deveroom.VisualStudio.Pro.UI.Dialogs
+{
+    public partial class WelcomeDialog
+    {
+        public WizardViewModel ViewModel { get; }
+
+        public WelcomeDialog()
+        {
+            InitializeComponent();
+        }
+
+        public WelcomeDialog(WizardViewModel viewModel, IVsUIShell vsUiShell = null) : base(vsUiShell)
+        {
+            ViewModel = viewModel;
+            InitializeComponent();
+        }
+    }
+}
