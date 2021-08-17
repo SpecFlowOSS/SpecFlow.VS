7c7796c34458a30afd7bb16577a539f083e4080f
Balázs Epresi <epresib@gmail.com>
2021-08-17 09:55:53 +0200

    Add SpecFlowInstallationStatus

---
+++ SpecFlow.VisualStudio/Analytics/IGuidanceConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpecFlow.VisualStudio.Analytics
+{
+    public interface IGuidanceConfiguration
+    {
+        GuidanceStep Installation { get; }
+
+        GuidanceStep Upgrade { get; }
+
+        IEnumerable<GuidanceStep> UsageSequence { get; }
+    }
+}
