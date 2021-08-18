c759a6ab98f9c24e8288c1019af5dada6f07f92e
Balázs Epresi <epresib@gmail.com>
2021-08-18 07:46:58 +0200

    Introduce DiscoveryException

---
+++ SpecFlow.VisualStudio.Common/DiscoveryException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SpecFlow.VisualStudio.Common
+{
+    public class DiscoveryException : Exception
+    {
+        public DiscoveryException()
+        {
+        }
+
+        public DiscoveryException(string message) : base(message)
+        {
+        }
+
+        public DiscoveryException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
