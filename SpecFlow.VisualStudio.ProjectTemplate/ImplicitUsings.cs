df6123284447ba19c290bd22f04fcc642f835703
Sandor Tardi <s.tardi@tricentis.com>
2021-11-22 11:19:04 +0100

    Update SpecFlow Project Templates for .NET 6 language features VS2022

---
+++ SpecFlow.VisualStudio.ProjectTemplate/ImplicitUsings.cs
@@ -0,0 +1,5 @@
+﻿global using TechTalk.SpecFlow;$if$ ('$unittestframework$' == 'NUnit')
+global using NUnit;$endif$$if$ ('$unittestframework$' == 'MSTest')
+global using Microsoft.VisualStudio.TestTools.UnitTesting;$endif$$if$ ('$unittestframework$' == 'xUnit')
+global using Xunit;$endif$$if$ ('$fluentassertionsincluded$' == 'True')
+global using FluentAssertions;$endif$
