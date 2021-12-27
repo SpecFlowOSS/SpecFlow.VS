d0db3d359655cee766a6f764210aa6d27d092e7c
Sandor Tardi <s.tardi@tricentis.com>
2021-12-27 11:32:33 +0100

    Code cleanup: #nullable enable in RegistryManager

--- SpecFlow.VisualStudio/Analytics/SpecFlowInstallationStatus.cs
+++ SpecFlow.VisualStudio/Analytics/SpecFlowInstallationStatus.cs
@@ -1,15 +1,15 @@
-#nullable disable
-
 namespace SpecFlow.VisualStudio.Analytics;
 
 public class SpecFlowInstallationStatus
 {
-    public bool IsInstalled => InstalledVersion != null;
-    public bool Is2019Installed => Installed2019Version != null;
-    public Version InstalledVersion { get; set; }
-    public Version Installed2019Version { get; set; }
-    public DateTime? InstallDate { get; set; }
-    public DateTime? LastUsedDate { get; set; }
+    public static readonly DateTime MagicDate = new(2009, 9, 11); //when SpecFlow has born
+    public static readonly Version UnknownVersion = new(0, 0);
+    public bool IsInstalled => InstalledVersion != UnknownVersion;
+    public bool Is2019Installed => Installed2019Version != UnknownVersion;
+    public Version InstalledVersion { get; set; } = UnknownVersion;
+    public Version Installed2019Version { get; set; } = UnknownVersion;
+    public DateTime InstallDate { get; set; }
+    public DateTime LastUsedDate { get; set; }
     public int UsageDays { get; set; }
     public int UserLevel { get; set; }
 }
