344548919d9e2c54384cafaf2f17526d5579c7c4
patiqs <s.tardi@tricentis.com>
2021-12-28 12:17:23 +0100

    Tricentis in registry manager (#48)
    * Change registry key from 'Software\TechTalk' to 'Software\Tricentis'
    
    * Code cleanup: #nullable enable in RegistryManager

--- SpecFlow.VisualStudio/Analytics/RegistryManager.cs
+++ SpecFlow.VisualStudio/Analytics/RegistryManager.cs
@@ -1,4 +1,3 @@
-#nullable disable
 namespace SpecFlow.VisualStudio.Analytics;
 
 public interface IRegistryManager
@@ -11,10 +10,12 @@ public interface IRegistryManager
 public class RegistryManager : IRegistryManager
 {
 #if DEBUG
-    private const string REG_PATH = @"Software\TechTalk\SpecFlow\Debug";
+    private static string RegPath => @"Software\Tricentis\SpecFlow\Debug";
 #else
-        private const string REG_PATH = @"Software\TechTalk\SpecFlow";
+    private static string RegPath => @"Software\Tricentis\SpecFlow";
 #endif
+
+    private static string RegPathFallback => @"Software\TechTalk\SpecFlow";
     private const string Version2019 = "version";
     private const string Version = "version.vs2022";
     private const string InstallDate = "installDate.vs2022";
@@ -22,30 +23,25 @@ public class RegistryManager : IRegistryManager
     private const string UsageDays = "usageDays";
     private const string UserLevel = "userLevel";
 
-    private string RegPath
-    {
-        get
-        {
-            var regPath = REG_PATH;
-            return regPath;
-        }
-    }
-
     public SpecFlowInstallationStatus GetInstallStatus()
     {
         var status = new SpecFlowInstallationStatus();
 
-        using (var key = Registry.CurrentUser.OpenSubKey(RegPath, RegistryKeyPermissionCheck.ReadSubTree))
+        try
         {
-            if (key == null)
-                return status;
+            using var key = Registry.CurrentUser.OpenSubKey(RegPath, RegistryKeyPermissionCheck.ReadSubTree)
+                            ?? Registry.CurrentUser.OpenSubKey(RegPathFallback, RegistryKeyPermissionCheck.ReadSubTree);
 
-            status.Installed2019Version = ReadStringValue(key, Version2019, s => new Version(s));
-            status.InstalledVersion = ReadStringValue(key, Version, s => new Version(s));
-            status.InstallDate = ReadIntValue(key, InstallDate, DeserializeDate);
-            status.LastUsedDate = ReadIntValue(key, LastUsedDate, DeserializeDate);
-            status.UsageDays = ReadIntValue(key, UsageDays, i => i);
-            status.UserLevel = ReadIntValue(key, UserLevel, i => i);
+            status.Installed2019Version = ReadVersion(key, Version2019);
+            status.InstalledVersion = ReadVersion(key, Version);
+            status.InstallDate = ReadDate(key, InstallDate);
+            status.LastUsedDate = ReadDate(key, LastUsedDate);
+            status.UsageDays = ReadIntValue(key, UsageDays);
+            status.UserLevel = ReadIntValue(key, UserLevel);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex, $"Registry read error: {this}");
         }
 
         return status;
@@ -53,70 +49,49 @@ public class RegistryManager : IRegistryManager
 
     public bool UpdateStatus(SpecFlowInstallationStatus status)
     {
-        using (var key = Registry.CurrentUser.CreateSubKey(RegPath, RegistryKeyPermissionCheck.ReadWriteSubTree))
-        {
-            if (key == null)
-                return false;
+        using var key = Registry.CurrentUser.CreateSubKey(RegPath, RegistryKeyPermissionCheck.ReadWriteSubTree);
 
-            if (status.InstalledVersion != null)
-                key.SetValue(Version, status.InstalledVersion);
-            if (status.InstallDate != null)
-                key.SetValue(InstallDate, SerializeDate(status.InstallDate));
-            if (status.LastUsedDate != null)
-                key.SetValue(LastUsedDate, SerializeDate(status.LastUsedDate));
-            key.SetValue(UsageDays, status.UsageDays);
-            key.SetValue(UserLevel, status.UserLevel);
-        }
+        if (key == null)
+            return false;
+
+        if (status.InstalledVersion != null)
+            key.SetValue(Version, status.InstalledVersion);
+        if (status.InstallDate != SpecFlowInstallationStatus.MagicDate)
+            key.SetValue(InstallDate, SerializeDate(status.InstallDate));
+        if (status.LastUsedDate != SpecFlowInstallationStatus.MagicDate)
+            key.SetValue(LastUsedDate, SerializeDate(status.LastUsedDate));
+        key.SetValue(UsageDays, status.UsageDays);
+        key.SetValue(UserLevel, status.UserLevel);
 
         return true;
     }
 
-    private T ReadStringValue<T>(RegistryKey key, string name, Func<string, T> converter)
+    private Version ReadVersion(RegistryKey key, string name)
     {
-        try
-        {
-            var value = key.GetValue(name) as string;
-            if (string.IsNullOrEmpty(value))
-                return default;
-            return converter(value);
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine(ex, $"Registry read error: {this}");
-            return default;
-        }
+        if (key.GetValue(name) is string value)
+            return new Version(value);
+        return SpecFlowInstallationStatus.UnknownVersion;
     }
 
-    private T ReadIntValue<T>(RegistryKey key, string name, Func<int, T> converter)
+    private DateTime ReadDate(RegistryKey key, string name)
     {
-        try
-        {
-            var value = key.GetValue(name);
-            if (value == null || !(value is int))
-                return default;
-            return converter((int) value);
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine(ex, $"Registry read error: {this}");
-            return default;
-        }
+        var value = key.GetValue(name);
+        return DeserializeDate((int) value);
     }
 
-    private readonly DateTime magicDate = new(2009, 9, 11); //when SpecFlow has born
+    private int ReadIntValue(RegistryKey key, string name)
+    {
+        var value = key.GetValue(name);
+        return (int) value;
+    }
 
-    private DateTime? DeserializeDate(int days)
+    private DateTime DeserializeDate(int days)
     {
         if (days <= 0)
-            return null;
-        return magicDate.AddDays(days);
+            return SpecFlowInstallationStatus.MagicDate;
+        return SpecFlowInstallationStatus.MagicDate.AddDays(days);
     }
 
-    private int SerializeDate(DateTime? dateTime)
-    {
-        if (dateTime == null)
-            return 0;
-
-        return (int) dateTime.Value.Date.Subtract(magicDate).TotalDays;
-    }
+    private int SerializeDate(DateTime dateTime) =>
+        (int) dateTime.Date.Subtract(SpecFlowInstallationStatus.MagicDate).TotalDays;
 }
