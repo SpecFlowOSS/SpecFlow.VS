879b7713020715b97f518e501f64c340d9d8f168
Gaspar Nagy <gaspar.nagy@gmail.com>
2022-02-22 17:12:46 +0100

    sample sf plugin

---
+++ Tests/Deveroom.SampleSpecFlow3940.SpecFlowPlugin/SampleRuntimePlugin.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Reflection;
+using Deveroom.SampleSpecFlow3940.SpecFlowPlugin;
+using TechTalk.SpecFlow.Bindings;
+using TechTalk.SpecFlow.Bindings.Discovery;
+using TechTalk.SpecFlow.Bindings.Reflection;
+using TechTalk.SpecFlow.Plugins;
+using TechTalk.SpecFlow.UnitTestProvider;
+
+[assembly: RuntimePlugin(typeof(SampleRuntimePlugin))]
+
+namespace Deveroom.SampleSpecFlow3940.SpecFlowPlugin
+{
+    public class SampleRuntimePlugin : IRuntimePlugin
+    {
+        public static bool Initialized = false;
+
+
+        public void Initialize(RuntimePluginEvents runtimePluginEvents, RuntimePluginParameters runtimePluginParameters,
+            UnitTestProviderConfiguration unitTestProviderConfiguration)
+        {
+            Initialized = true;
+            runtimePluginEvents.CustomizeGlobalDependencies +=
+                (_, args) =>
+                {
+                    args.ObjectContainer.RegisterInstanceAs(this);
+                    args.ObjectContainer.RegisterTypeAs<CustomBindingRegistryBuilder, IRuntimeBindingRegistryBuilder>();
+                };
+        }
+
+        public class CustomBindingRegistryBuilder : IRuntimeBindingRegistryBuilder
+        {
+            private readonly RuntimeBindingRegistryBuilder _innerBuilder;
+            private readonly IBindingRegistry _bindingRegistry;
+
+            public CustomBindingRegistryBuilder(RuntimeBindingRegistryBuilder innerBuilder, IBindingRegistry bindingRegistry)
+            {
+                _innerBuilder = innerBuilder;
+                _bindingRegistry = bindingRegistry;
+            }
+
+            public void BuildBindingsFromAssembly(Assembly assembly)
+            {
+                _innerBuilder.BuildBindingsFromAssembly(assembly);
+            }
+
+            public void BuildingCompleted()
+            {
+                _bindingRegistry.RegisterStepDefinitionBinding(new StepDefinitionBinding(
+                    StepDefinitionType.Then, "there should be a step from a plugin", 
+                    new RuntimeBindingMethod(typeof(PluginSteps).GetMethod(nameof(PluginSteps.ThenThereShouldBeAStepFromAPlugin))), null));
+
+                _innerBuilder.BuildingCompleted();
+            }
+        }
+    }
+
+    public class PluginSteps
+    {
+        public void ThenThereShouldBeAStepFromAPlugin()
+        {
+            Console.WriteLine("Hello from plugin!");
+        }
+    }
+}
