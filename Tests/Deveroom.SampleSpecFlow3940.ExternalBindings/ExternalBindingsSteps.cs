f7f6ed918f74c5c26693a0fc5596a00fdedc622d
Gaspar Nagy <gaspar.nagy@gmail.com>
2022-02-22 16:55:39 +0100

    sample ext lib

---
+++ Tests/Deveroom.SampleSpecFlow3940.ExternalBindings/ExternalBindingsSteps.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Deveroom.SampleSpecFlow3940.ExternalBindingsX
+{
+    [Binding]
+    public class ExternalBindingsSteps
+    {
+        [Then("there should be a step from an external assembly")]
+        public void ThenThereShouldBeAStepFromAnExternalAssembly()
+        {
+            Console.WriteLine("Hello from external assembly!");
+        }
+    }
+}
